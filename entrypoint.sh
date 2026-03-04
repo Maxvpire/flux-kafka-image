cca121e35c8e702a6ad1f40b3c3697a1e65f77b0
Maxvpire <maxvpire@gmail.com>
2026-03-04 15:31:01 +0500

    all

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+HOST="${RENDER_EXTERNAL_HOSTNAME:-localhost}"
+export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://${HOST}:9092"
+
+echo "==> Advertising Kafka on: ${KAFKA_ADVERTISED_LISTENERS}"
+
+exec /opt/kafka/scripts/setup.sh
